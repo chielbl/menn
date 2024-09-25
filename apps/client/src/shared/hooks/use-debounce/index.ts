8195a9ec7e6f0c2081339cd8be86935a9f7384cc
chielbl <chielbleyenbergh@gmail.com>
2024-09-25 22:11:43 +0200

    Fix variable initialization in useDebounce hook

--- apps/client/src/shared/hooks/use-debounce/index.ts
+++ apps/client/src/shared/hooks/use-debounce/index.ts
@@ -1,6 +1,6 @@
 export function useDebounce() {
   const debounce = (fn: () => void, delay = 500) => {
-    let timeoutId: NodeJS.Timeout;
+    let timeoutId: NodeJS.Timeout | undefined;
     if (timeoutId) clearTimeout(timeoutId);
     timeoutId = setTimeout(() => fn(), delay);
   };
