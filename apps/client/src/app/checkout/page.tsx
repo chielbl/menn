55f00be8161539a84a6fe4ba4fe9f509994a7db9
chielbl <chielbleyenbergh@gmail.com>
2024-11-01 17:25:12 +0100

    Update form layout and basket width in checkout pageand basket component styling

--- apps/client/src/app/checkout/page.tsx
+++ apps/client/src/app/checkout/page.tsx
@@ -1,7 +1,25 @@
 import { BasePage } from '@/shared/components';
 
 function CheckoutPage() {
-  return <BasePage pageName="Checkout"><>hi</></BasePage>;
+  return (
+    <BasePage pageName="Checkout">
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" placeholder='Enter your full name'/>
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name="email" placeholder='Enter your email'/>
+        </div>
+        <div>
+          <label htmlFor="phone">Email:</label>
+          <input type="text" id="phone" name="phone" placeholder='Enter your phone n
+          umber'/>
+        </div>
+      </form>
+    </BasePage>
+  );
 }
 
 export default CheckoutPage;
